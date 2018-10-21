29f455cd8a62144942c2278f7abbf97df5b56aaa
BrettSWeisberg <weisberg.brett@gmail.com>
2018-10-21 19:38:29 -0400

    Project controller built

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,38 @@
 class ProjectsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create # new users
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      redirect_to new_project_path
+    end
+  end
+
+  def show
+    if session[:manager_id]
+      @project = Project.find(params[:id])
+    else
+      redirect_to '/'
+    end
+  end
+
+  def update
+
+  end
+
+private
+
+  def project_params
+    params.require(:project).permit(:, :)
+  end
+
+end
 end
