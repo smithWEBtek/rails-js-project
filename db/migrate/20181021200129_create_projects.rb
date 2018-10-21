cefc96764430c64b90f335533cf65b5eb4312edc
BrettSWeisberg <weisberg.brett@gmail.com>
2018-10-21 17:07:48 -0400

    Built controllers

--- db/migrate/20181021200129_create_projects.rb
+++ db/migrate/20181021200129_create_projects.rb
@@ -2,6 +2,8 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :name
+      t.boolean :completed
+
 
       t.timestamps null: false
     end
