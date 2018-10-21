6caf1be90b5528d0ace8d380cf9989aeb2e692e9
BrettSWeisberg <weisberg.brett@gmail.com>
2018-10-21 17:44:04 -0400

    Client controllers built

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,38 @@
 class ClientsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def create # new users
+    @client = Client.new(client_params)
+    if @client.save
+      session[:client_id] = @client.id
+      redirect_to client_path(@client)
+    else
+      redirect_to new_client_path
+    end
+  end
+
+  def show
+    if session[:client_id]
+      @client = Client.find(params[:id])
+    else
+      redirect_to '/'
+    end
+  end
+
+  def update
+
+  end
+
+private
+
+  def client_params
+    params.require(:client).permit(:name, :password, :nausea, :happiness, :tickets, :height, :admin)
+  end
+
 end
