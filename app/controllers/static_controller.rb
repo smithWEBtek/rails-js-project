3fda1c218a59dd74addb2a4bb695ae47ff51fd4f
BrettSWeisberg <weisberg.brett@gmail.com>
2018-10-24 06:31:34 -0400

    Home page update

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,10 @@
 class StaticController < ApplicationController
   def home
-    if session[:manager_id]
-      @Manager = Manager.find(session[:manager_id])
+    if session[:manager_id] != nil
+      @current_user = Manager.find(session[:manager_id])
+    else session[:client_id]
+      @client = Client.find(session[:client_id])
     end
+
   end
 end
