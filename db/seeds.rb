18249d9685a3c94afa961dfa528eac2b679e9dd8
BrettSWeisberg <weisberg.brett@gmail.com>
2018-10-24 12:51:33 -0400

    Seed Creation

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+DATA = {
+  :clients => [
+    ["Max Charles"],
+    ["Skai Jackson"],
+    ["Kaleo Elam"],
+    ["Megan Charpentier"],
+  ],
+  :managers => [
+    ["Hayden Byerly",  "HB@email" "password"],
+    ["Tenzing Norgay Trainor", "TNT@email", "password"],
+    ["Davis Cleveland", "DC@email" , "password"],
+    ["Cole Sand", "CS@email", "password"],
+    ["Quvenzhané Wallis", "QW@email", "password"]
+  ],
+  :projects => [
+    ["Project1", Yes, 1, 2],
+    ["Project2", No, 3, 4],
+    ["Project3", Yes, 1, 3],
+    ["Project4", No, 1, 4]
+  ]
+}
