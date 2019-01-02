0272a0c28bdfbfde42763a18b46579f8fcd352f6
Brett Weisberg <weisberg.brett@gmail.com>
2019-01-01 19:18:48 -0500

    Stylesheets restart

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,19 +1,18 @@
 
 
+//
 
+//function showClients() {
+      $("#DisplayClients").on("click",function() {
+        alert("YO!");
 
-function showClients() {
-  $(document).ready(function(){
-      $(`#DisplayClients`).on("click",function(){
-        debugger
-        $.get(`/managers`).done (function(state){
-          $('#clients').appendeach(function(i, element){
-             element.innerHTML = state.data.attributes.state[i];
-           })
-         })
-       })
-  });
- }
+  //      $.get(`/managers`).done (function(state){
+    //      $('#clients').appendeach(function(i, element){
+      //       element.innerHTML = state.data.attributes.state[i];
+        //   })
+      //   })
+    });
+ //}
 //   // @manager.clients.
 
 // function showClients() {
