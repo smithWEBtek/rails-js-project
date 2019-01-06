09b984683ec22e2b3d5e0090a021205c2c93f5a5
Brett Weisberg <weisberg.brett@gmail.com>
2019-01-06 11:12:38 -0500

    Project Show page JS function set up -start

--- app/assets/javascripts/manager.js
+++ app/assets/javascripts/manager.js
@@ -1,5 +1,5 @@
 $(function (){
-  listenToForm()
+  listenToForm();
 })
 
 const base_url = 'http://localhost:3000/'
@@ -7,8 +7,8 @@ const base_url = 'http://localhost:3000/'
 function listenToForm (){
   $("#DisplayClients").on('click', function(event) {
     event.preventDefault();
-  //  debugger
     getClients();
+
   })
 }
 
@@ -27,7 +27,6 @@ function getClients(){
           `<ul id="show_projects-${myObj[x].id}"></ul>`;
       }
       listenToButton(this);
-      //console.log("the response:",response);
     }
   })
 }
@@ -41,7 +40,6 @@ function listenToButton (){
 
 
 function getProjects(data){
-//  console.log("data");
   let client = Number(data.id.split("-")[1]);
   let project_ul = `show_projects-${client}`;
   let project_show_url = base_url + 'clients/' + client + '/projects/'
@@ -52,12 +50,9 @@ function getProjects(data){
     method: 'get',
     dataType: 'JSON',
     success: function(response) {
-
       let myObj = response.projects;
-
       for (x in myObj) {
         if (myObj[x].client_id === client) {
-          //debugger
           document.getElementById(`show_projects-${client}`).innerHTML += `<li>Project Name: <a href=`+ project_show_url + myObj[x].id +`>` + myObj[x].name + '</a></li>';
         }
       }
--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,31 +1,31 @@
-// $(function (){
-//   listenToPage()
-// })
-//
-//
-// function listenToPage (){
-//   $("#Next").on('click', function(event) {
-//     event.preventDefault();
-//   //  debugger
-//     getProjects();
-//   })
-// }
-//
-// function getProjects(){
-//   $.ajax({
-//     url: this.url.split('/projects')[0], //data .json if you dont use dataType
-//     method: 'get',
-//     dataType: 'JSON',
-//     success: function(response) {
-//       debugger
-//       let myObj = response.projects;
-//       for (x in myObj) {
-//         document.getElementById("").innerHTML += '<li>Name: ' + myObj[x].name + '<br>' +
-//         `<button id="clientid-${myObj[x].id}">Display Projects</button><br>` +
-//           `<ul id="show_projects-${myObj[x].id}"></ul>`;
-//       }
-//       listenToButton(this);
-//       //console.log("the response:",response);
-//     }
-//   })
-// }
+$(function (){
+   console.log("only runs on the contact page!");
+   listenToProjectPage();
+ })
+
+
+ function listenToProjectPage(){
+   $("#Next").on('click', function(event) {
+     event.preventDefault();
+     console.log("hello!")
+     getDetails();
+  })
+
+
+function getDetails(){
+  $.ajax({
+    url: this.location.href.split('/projects')[0], //data .json if you dont use dataType
+    method: 'get',
+    dataType: 'JSON',
+    success: function(response) {
+      debugger
+      let myObj = response.projects;
+      for (x in myObj) {
+        document.getElementById("#project_name").innerHTML += myObj[x].name
+      //  document.getElementById("#manager_name").innerHTML += myObj[x].manager_id;
+        //document.getElementById("#client_name").innerHTML += myObj[x].client_id;
+      //  document.getElementById("#completed").innerHTML += myObj[x].completed;
+      }
+    }
+  })
+}
